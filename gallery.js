dabb161a1c9760a35c37c56c0c06e46f7ec212e3
Mercy <mercycherotich757@gmail.com>
2022-01-19 12:00:58 +0300

    corrected hover on all images

--- gallery.js
+++ gallery.js
@@ -10,4 +10,81 @@ $(document).ready(function() {
         }, 900);
         $('.text1').addClass('hide1');
     });
+    $('#part2 a img').hover(function() {
+        $(this).stop().animate({
+            opacity: .7
+        }, 700);
+        $('.text2').removeClass('hide2');
+    }, function() {
+        $(this).stop().animate({
+            opacity: 1
+        }, 900);
+        $('.text2').addClass('hide2');
+    });
+    $('#part3 a img').hover(function() {
+        $(this).stop().animate({
+            opacity: .7
+        }, 700);
+        $('.text3').removeClass('hide3');
+    }, function() {
+        $(this).stop().animate({
+            opacity: 1
+        }, 900);
+        $('.text3').addClass('hide3');
+    });
+    $('#part4 a img').hover(function() {
+        $(this).stop().animate({
+            opacity: .7
+        }, 700);
+        $('.text4').removeClass('hide4');
+    }, function() {
+        $(this).stop().animate({
+            opacity: 1
+        }, 900);
+        $('.text4').addClass('hide4');
+    });
+    $('#part5 a img').hover(function() {
+        $(this).stop().animate({
+            opacity: .7
+        }, 700);
+        $('.text5').removeClass('hide5');
+    }, function() {
+        $(this).stop().animate({
+            opacity: 1
+        }, 900);
+        $('.text5').addClass('hide5');
+    });
+    $('#part6 a img').hover(function() {
+        $(this).stop().animate({
+            opacity: .7
+        }, 700);
+        $('.text6').removeClass('hide6');
+    }, function() {
+        $(this).stop().animate({
+            opacity: 1
+        }, 900);
+        $('.text6').addClass('hide6');
+    });
+    $('#part7 a img').hover(function() {
+        $(this).stop().animate({
+            opacity: .7
+        }, 700);
+        $('.text7').removeClass('hide7');
+    }, function() {
+        $(this).stop().animate({
+            opacity: 1
+        }, 900);
+        $('.text7').addClass('hide7');
+    });
+    $('#part8 a img').hover(function() {
+        $(this).stop().animate({
+            opacity: .7
+        }, 700);
+        $('.text8').removeClass('hide8');
+    }, function() {
+        $(this).stop().animate({
+            opacity: 1
+        }, 900);
+        $('.text8').addClass('hide8');
+    });
 });
